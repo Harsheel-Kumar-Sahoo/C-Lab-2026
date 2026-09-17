340f2b267461157b0fcc5a5ed8c75ef42fc9acb9
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 20:17:05 +0530

    Create Q21.c

---
+++ Lab01/Q21.c
@@ -0,0 +1,34 @@
+// PROGRAM TO READ TWO NOS. AND PERFORM SPECIFIC TASK (USING ARITHMETIC OPERATOR)AND THIS PERFORM USING SWITCH-CASE.(CASE IN CHAR FORM).
+#include <stdio.h>
+
+int main(){
+    float num1, num2;
+    char op;
+
+    printf("Enter the first number : ");
+    scanf("%f", &num1);
+
+    printf("Enter the second number : ");
+    scanf("%f", &num2);
+    
+    printf("Enter the operator [+, -, *, /] : ");
+    scanf(" %c", &op);
+
+    printf(" ");
+
+    switch (op) {
+        case '+' :
+            printf("%f + %f = %f", num1, num2, num1 + num2);
+            break;
+        case '-' :
+            printf("%f - %f = %f", num1, num2, num1 - num2);
+            break;
+        case '*' :
+            printf("%f * %f = %f", num1, num2, num1 * num2);
+            break;
+        case '/' :
+            printf("%f / %f = %f", num1, num2, num1 / num2);
+            break;
+    }
+    return 0;
+}
