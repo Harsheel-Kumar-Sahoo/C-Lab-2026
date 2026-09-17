59c4b01f9c23c8e474eb184828cefa51686caeb9
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 18:07:46 +0530

    Create Q1.c

---
+++ Lab01/Q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    float p, r, t;
+    printf("Enter the principle amount:");
+    scanf("%f", &p);
+    printf("Enter the rate of interest:");
+    scanf("%f", &r);
+    printf("Enter the time period (in years):");
+    scanf("%f", &t);
+    float SI = (p * r * t) / 100;
+    printf("The simple interest is: %f\n", SI);
+    
+    
+    return 0;
+}
