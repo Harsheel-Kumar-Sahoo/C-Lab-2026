f9c13f49c5d0cd094e7ecfe69f23187f074822c1
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 20:41:51 +0530

    Implement max and second max finder program
    The program prompts the user for 'N' numbers and finds the maximum and second maximum values among them.

---
+++ Lab01/Q32.c
@@ -0,0 +1,28 @@
+// PROGRAM TO FINDOUT MAX AND SECOND MAX FROM ‘N’ NOS .
+#include <stdio.h>
+
+int main(){
+    int max, secondMax, n;
+    printf("How many numbers to add : ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        int num;
+        printf("Enter the number : ");
+        scanf("%d", &num);
+        if (i == 0) {
+            max = num;
+        } else {
+            secondMax = num;
+            if (secondMax > max) {
+                int temp = max;
+                max = secondMax;
+                secondMax = temp;
+            }
+        }
+    } 
+
+    printf("Max element : %d\n", max);
+    printf("Second Max element : %d", secondMax);
+
+    return 0;
+}
