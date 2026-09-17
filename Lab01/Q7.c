dff2385a8a9f74ee9f643e2e075814c80812fcfa
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 18:31:44 +0530

    Add program to calculate area of a triangle

---
+++ Lab01/Q7.c
@@ -0,0 +1,15 @@
+// PROGRAM TO CALCULATE AREA OF A TRIANGLE.
+#include <stdio.h>
+
+int main(){
+    float height, base, area;
+    printf("Enter the value of height of triangle : ");
+    scanf("%f", &height);
+    printf("Enter the value of base of triangle : ");
+    scanf("%f", &base);
+
+    area = 0.5 * height * base;
+
+    printf("The area of the triangle is %f", area);
+    return 0;
+}
