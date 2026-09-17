d749a8ea96f9c20cfeeddf1889ec3fbc0f1e7754
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 18:53:41 +0530

    Create Q17.c

---
+++ Lab01/Q17.c
@@ -0,0 +1,25 @@
+// PROGRAM TO READ THREE NOS. AND PRINT MAX USING LOGICAL OPERATOR.(&&)
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+
+    printf("Enter the first number : ");
+    scanf("%d", &a);
+    
+    printf("Enter the second number : ");
+    scanf("%d", &b);
+    
+    printf("Enter the third number : ");
+    scanf("%d", &c);
+
+    if (a >= b && a >= c) {
+        printf("Max : %d", a);
+    } else if (b >= a && b >= c) {
+        printf("Max : %d", b);
+    } else {
+        printf("Max : %d", c);
+    }
+    
+    return 0;
+}
