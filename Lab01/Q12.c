e93316fd9faaa68c6572ecab110db6a998c2f9a2
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 18:44:13 +0530

    Create Q12.c

---
+++ Lab01/Q12.c
@@ -0,0 +1,12 @@
+// PROGRAM TO PRINT ASCII CODE OF ANY CHARACTER.
+#include <stdio.h>
+
+int main(){
+    int value;
+    char c;
+    printf("Enter the character : ");
+    scanf("%c", &c);
+    value = c;
+    printf("The ASCII value is %d \n", value);
+    return 0;
+}
