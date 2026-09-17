df31fa0f23f2808de8e118be4bd3037ae15238f6
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 20:28:41 +0530

    Create Q25.c

---
+++ Lab01/Q25.c
@@ -0,0 +1,14 @@
+// PROGRAM TO PRINT TABLE OF ANY NO.
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter the number :");
+    scanf("%d", &num);
+    for (int i = 1; i < 11; i++)
+    {
+        printf("%d x %d = %d \n", num, i, num * i);
+        
+    }
+    return 0;
+}
