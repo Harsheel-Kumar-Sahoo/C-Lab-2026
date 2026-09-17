c56854af379d1c20b58979fd99c87c514ec60a59
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 20:57:11 +0530

    Create Q37.c

---
+++ Lab01/Q37.c
@@ -0,0 +1,14 @@
+// PROGRAM TO PRINT 2,4,6,8,10,12.........N.
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number till which you want odd numbers: ");
+    scanf("%d", &n);
+    int i = 2;
+    while (i <= n) {
+        printf("%d ", i);
+        i += 2;
+    }
+    return 0;
+}
