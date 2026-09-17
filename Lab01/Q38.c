a94975582d9a43c1ac70d0e2b470e7e5898d69b5
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-17 21:02:05 +0530

    Update Q38.c

--- Lab01/Q38.c
+++ Lab01/Q38.c
@@ -5,7 +5,7 @@ int main(){
     int n;
     printf("Enter the value of n : ");
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         printf("%d ", i * i);
     }
     
