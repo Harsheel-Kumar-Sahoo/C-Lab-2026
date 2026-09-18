da74537a787b0580d9eae8f044e9b6c4b8260875
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-18 14:44:32 +0530

    Create Q37.c

---
+++ Lab02/Q37.c
@@ -0,0 +1,24 @@
+/* Print Patterm
+    1
+   12
+  123
+ 1234
+12345
+*/
+#include <stdio.h>
+
+int main(){
+    int rows;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < (rows - i - 1); j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= (i + 1); j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
