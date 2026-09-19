db9c16a0ad42b5a143558b7cd58158b8597874b5
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-19 05:39:39 +0530

    Update Q30.c

--- Lab01/Q30.c
+++ Lab01/Q30.c
@@ -1,23 +1,18 @@
 // PROGRAM TO REVERSE OF A GIVEN NO.
 #include <stdio.h>
-#include <math.h>
 
-int main(){
+int main() {
     int num;
     printf("Enter the number : ");
     scanf("%d", &num);
-    int revNum = 0;
-    int numCopy = num;
-    int power = log10(num) ;
 
-    while (numCopy != 0) {
-        int lastDigit = numCopy % 10;
-        numCopy = numCopy / 10;
-        revNum += lastDigit * pow(10, power);
-        power--;
+    int revNum = 0;
+    while (num != 0) {
+        revNum = revNum * 10 + num % 10;
+        num = num / 10;
     }
 
     printf("%d \n", revNum);
-    
+
     return 0;
 }
