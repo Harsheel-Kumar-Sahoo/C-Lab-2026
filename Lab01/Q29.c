ed5ec0ebebbc7cd42a9b6ad88eb0116ae0c48899
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-19 15:27:19 +0530

    Update Q29.c

--- Lab01/Q29.c
+++ Lab01/Q29.c
@@ -1,26 +1,20 @@
 // PROGRAM TO CHECK WHETHER A NO IS PALINDROM OR NOT.
 #include <stdio.h>
-#include <math.h>
 
 int main(){
-    int num;
+    int num, revNum = 0;
     printf("Enter the number : ");
     scanf("%d", &num);
-    int revNum = 0;
     int numCopy = num;
-    int power = log10(num) ;
-
     while (numCopy != 0) {
         int lastDigit = numCopy % 10;
-        numCopy = numCopy / 10;
-        revNum += lastDigit * pow(10, power);
-        power--;
+        numCopy /= 10;
+        revNum = (revNum * 10) + lastDigit;
     }
-
-    if (revNum == num) {
-        printf("It is a palindrome number \n");
+    if (num == revNum) {
+        printf("It is a palindrome number");
     } else {
-        printf("It is not a palindrome number \n");
+        printf("It is not a palindrome number");
     }
     return 0;
 }
