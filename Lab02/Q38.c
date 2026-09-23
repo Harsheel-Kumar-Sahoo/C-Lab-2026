e82fe49858160b3d04aa3c6360590e8742c6efcd
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-23 16:57:39 +0530

    Update Q38.c

--- Lab02/Q38.c
+++ Lab02/Q38.c
@@ -1,9 +1,9 @@
 /* Print Pattern
-*
-   **
-***
- ****
-*****
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
 */
 #include <stdio.h>
 
@@ -11,22 +11,16 @@ int main(){
     int rows;
     printf("Enter the number of rows : ");
     scanf("%d", &rows);
+
     for (int i = 0; i < rows; i++) {
-        if (i % 2 == 0) {
-            for (int j = 0; j < (i + 1); j++) {
-                printf("*");
-            }
-            for (int j = 0; j < (rows - i - 1); j++) {
-                printf(" ");
-            }
-        } else {
-            
-            for (int j = 0; j < (rows - i - 1); j++) {
-                printf(" ");
-            }
-            for (int j = 0; j < (i + 1); j++) {
-                printf("*");
-            }
+        for (int j = 0; j < (rows - i); j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < ((2 * i) + 1); j++){
+            printf("*");
+        }
+        for (int j = 0; j < (rows - i); j++) {
+            printf(" ");
         }
         printf("\n");
     }
