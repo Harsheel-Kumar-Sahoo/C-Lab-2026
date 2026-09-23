aeed352045c24b565ccd53f2fd35574bee46881a
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-23 16:38:43 +0530

    Update Q37.c

--- Lab02/Q37.c
+++ Lab02/Q37.c
@@ -12,9 +12,6 @@ int main(){
     printf("Enter the number of rows : ");
     scanf("%d", &rows);
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < (rows - i - 1); j++) {
-            printf(" ");
-        }
         for (int j = 1; j <= (i + 1); j++) {
             printf("%d", j);
         }
