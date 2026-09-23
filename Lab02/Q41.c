c1d715d337a74b76c55abb8d33bb779534571f42
Harsheel Kumar Sahoo <88924829+Harsheel-Kumar-Sahoo@users.noreply.github.com>
2026-09-23 16:43:55 +0530

    Update Q41.c

--- Lab02/Q41.c
+++ Lab02/Q41.c
@@ -1,8 +1,8 @@
 /* Print Pattern
-    1
-   121
-  12321
- 1234321
+1
+121
+12321
+1234321
 123454321
 */
 #include <stdio.h>
@@ -12,17 +12,11 @@ int main(){
     printf("Enter the number of rows : ");
     scanf("%d", &rows);
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < (rows - i -1); j++) {
-            printf(" ");
-        }
         for (int j = 1; j <= (i + 1); j++) {
             printf("%d", j);
         }
         for (int j = i; j >= 1; j--) {
             printf("%d", j);
-        }
-        for (int j = 0; j < (rows - i -1); j++) {
-            printf(" ");
         }
         printf("\n");
     }
